1357a1bf6f7964262a2ae634a0aaa0c1a3887eb4
taimoor1Abbasi <110571850+taimoor1Abbasi@users.noreply.github.com>
2022-08-08 20:50:30 -0400

    Add files via upload

---
+++ cosewic.c
@@ -0,0 +1,66 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+
+#include "cosewic.h"
+
+//MAIN FUNCTIONS
+void mainLogic(const struct Filedata data[], int records) {
+    int selection;
+    do
+    {
+        ("=======================================================\n");
+        printf("         Canada Species at Risk Analysis\n");
+        printf("            (Data Years: 2010 - 2019)\n");
+        printf("=======================================================\n");
+        printf("1. View by year (sorted ASCENDING by year)\n");
+        printf("2. View by province (sorted DESCENDING by totals)\n");
+        printf("3. View by taxon (sorted ASCENDING by totals)\n");
+        printf("4. View by province and taxon\n");
+        printf("-------------------------------------------------------\n");
+        printf("0. Exit\n");
+        printf("-------------------------------------------------------\n");
+        printf("Selection: \n");
+        switch (selection)
+        {
+        case 0:
+            printf("Application Terminated!\n");
+            break;
+        case 1:
+            ViewbyYear();
+            break;
+        case 2:
+            ViewbyProvince();
+            break;
+        case 3:
+            ViewbyTaxon();
+            break;
+        case 4:
+            Viewbypt();
+            break;
+        }
+    } while (selection); 
+
+
+}
+
+void loadData(const char* datafile, struct Filedata data[], int max) {
+}
+
+//Display FUNCTIONS
+void displayHeader(int type) {
+}
+
+
+//Appilcation Funtions
+ViewbyYear();
+
+ViewbyProvince();
+
+ViewbyTaxon();
+
+Viewbypt();
+
+//UTILITY FUNCTIONS
+int getyear(const struct Filedata data[]) {
+}
---
+++ cosewic.h
@@ -0,0 +1,32 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#define CHAR_LENGHT 30
+struct FileData
+{
+	int year;
+	char taxon[CHAR_LENGHT];
+	char status[CHAR_LENGHT];
+	int count;
+	char status[CHAR_LENGHT];
+};
+
+void mainLogic(const struct Filedata data[],int records);
+
+void loadData(const char* datafile,struct Filedata data[], int max);
+
+int getyear(const struct Filedata data[]);
+
+
+void displayHeader(int type);
+
+
+
+
+//Appilcation Funtions
+ViewbyYear();
+
+ViewbyProvince();
+
+ViewbyTaxon();
+
+Viewbypt();
